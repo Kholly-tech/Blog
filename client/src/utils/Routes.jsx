091041ba93041b36a1256a3ab1511c0938e3f88c
Kolawole Akintayo <kolawoleakintayok@gmail.com>
2025-02-10 04:36:40 -0800

    Init 2

---
+++ client/src/utils/Routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const UnProtectedRoutes = () => {
+  
+}
+const ProtectedRoutes = () => {
+  const [loading, setLoading] = useState(true);
+}
+const Routes = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Routes
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('@tailwindcss/postcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
       extend: {
-        colors: {
-          primary: "#00897B",
-          secondary: "#FFB74D",
-          lightGy: "#F2F2F2",
-          black: "#202020",
-          semiBlack: "#2D2D2D",
-          lightBlack: "#474747",
-          darkWhite: "#BEBEBE",
-          white: "#FFFFFF",
-          semiWhite: "#E6E6E6",
-          bg: "#FBE9E7",
-        },
       },
     },
     plugins: [],
